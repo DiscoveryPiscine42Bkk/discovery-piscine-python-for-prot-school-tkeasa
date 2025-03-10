02e075b05d20423db633a5d6d4bdbe1cee22f56f
tkeasa <42901@prot.ac.th>
2025-03-10 15:28:57 +0700

    Add files via upload

---
+++ cell02/ex01/isneg.py
@@ -0,0 +1,9 @@
+number = int(input("Please Enter your fuckin Number"))
+if number < 0 :
+    print("This Number fucking negative, no pregnant ")
+
+elif number > 0 :
+      print("This Number IS POSITIVE ,CONGRAT YOUR PREGNANT")
+
+elif number == 0 :
+    print("I cannot calculate because you fucking PUT ZERO")
