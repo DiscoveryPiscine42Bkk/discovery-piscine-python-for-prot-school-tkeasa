499a4af611463e29ef2240943c265bb532476348
tkeasa <42901@prot.ac.th>
2025-03-13 10:20:05 +0700

    Add files via upload

---
+++ cell04/ex01/ex02/calculator.py
@@ -0,0 +1,9 @@
+first_number = float(input("Give me the first number: "))
+second_number = float(input("Give me the second number: "))
+
+print("Fuck you! i won't do this shit again!")
+
+print(f"{first_number} + {second_number} = {first_number + second_number}")
+print(f"{first_number} - {second_number} = {first_number - second_number}")
+print(f"{first_number} / {second_number} = {first_number / second_number if second_number != 0 else 'Fuck you, its zero'}")
+print(f"{first_number} * {second_number} = {first_number * second_number}")
